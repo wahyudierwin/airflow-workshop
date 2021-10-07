6ba3bbd8bf691b4266206e30bc14889c7a310af3
Erwin Eko Wahyudi <erwin.eko@bukalapak.com>
2021-10-07 22:06:38 +0700

    Add file sensor

---
+++ dags/file_sensor.py
@@ -0,0 +1,21 @@
+from os.path import exists
+
+from airflow.operators.sensors import BaseSensorOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class FileSensor(BaseSensorOperator):
+    """
+    Menunggu suatu file apakah sudah ada.
+    """
+    template_fields = ('path', )
+
+    @apply_defaults
+    def __init__(self, path, *args, **kwargs):
+        self.path = path
+        kwargs.setdefault('poke_interval', 10)
+        super(FileSensor, self).__init__(*args, **kwargs)
+
+    def poke(self, _context):
+        print('poking path', self.path)
+        return exists(self.path)
