493d06b2578002fff5929c94098cbd24e9f12ba4
Erwin Eko Wahyudi <erwin.eko@bukalapak.com>
2021-12-15 15:36:01 +0700

    add dag latest

--- dags/a_workshop_complex_latest_dag.py
+++ dags/a_workshop_complex_latest_dag.py
@@ -7,7 +7,7 @@ from airflow.utils.dates import days_ago
 from file_sensor import FileSensor
 
 dag = DAG(
-    dag_id='a_workshop_complex_dag',
+    dag_id='a_workshop_complex_latest_dag',
     schedule_interval='0 0 * * *',
     start_date=days_ago(3),
     dagrun_timeout=timedelta(minutes=60),
@@ -15,34 +15,38 @@ dag = DAG(
 )
 
 project_dir = '{{var.json.workshop.project_dir}}'
-data_dir = '{{var.json.workshop.data_dir}}/{{ds}}'
+data_dir = '{{var.json.workshop.data_dir}}'
 
 wait_users_data = FileSensor(
     task_id='wait_users_data',
-    path=f'{data_dir}/users.dat',
+    path=f'{data_dir}/{{{{ds}}}}/users.dat',
+    execution_timeout=timedelta(minutes=5),
     dag=dag
 )
 
 wait_movies_data = FileSensor(
     task_id='wait_movies_data',
-    path=f'{data_dir}/movies.dat',
+    path=f'{data_dir}/{{{{ds}}}}/movies.dat',
+    execution_timeout=timedelta(minutes=5),
     dag=dag
 )
 
 wait_ratings_data = FileSensor(
     task_id='wait_ratings_data',
-    path=f'{data_dir}/ratings.dat',
+    path=f'{data_dir}/{{{{ds}}}}/ratings.dat',
+    execution_timeout=timedelta(minutes=5),
     dag=dag
 )
 
 analyze = BashOperator(
     task_id='analyze',
     bash_command=f'python' \
-        f' {project_dir}/codes/analyze.py'
-        f' --users-path {data_dir}/users.dat'
-        f' --movies-path {data_dir}/movies.dat'
-        f' --ratings-path {data_dir}/ratings.dat'
+        f' {project_dir}/codes/analyze_latest.py'
+        f' --users-latest-path {data_dir}/latest/users.json'
+        f' --movies-latest-path {data_dir}/latest/movies.json'
+        f' --ratings-latest-path {data_dir}/latest/ratings.json'
         f' --output-path {data_dir}/grouped.csv',
+    trigger_rule='all_done',
     dag=dag
 )
 
